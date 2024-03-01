a70bd967c600db9a555f7488c11ee4b13787b6c9
0x902 <ui.yazir@gmail.com>
2024-03-02 02:51:32 +0530

    initial commit. basic features and website layout.

---
+++ app.js
@@ -0,0 +1,98 @@
+const elements = {
+    urlInput: document.querySelector(".url-input"),
+    qrEl: document.querySelector(".qrcode-container"),
+    foreGroundColorEl: document.querySelector(".foreground-input"),
+    backgroundColorEl: document.querySelector(".background-input"),
+    logoInputEl: document.querySelector(".logo-input"),
+    alertUrlEl: document.querySelector(".alert-url"),
+    alertColorsEl: document.querySelector(".alert-colors"),
+    generateBtnEl: document.querySelector(".btn-generate"),
+    ulrPreviewEl: document.querySelector(".url"),
+    themesEl: document.querySelectorAll(".theme"),
+};
+
+let foreGroundColor = elements.foreGroundColorEl.value;
+let backgroundColor = elements.backgroundColorEl.value;
+let siteUrl = "";
+let validURl = false;
+let validColors = true;
+
+function updateColors() {
+    if (isValidHex(foreGroundColor) && isValidHex(backgroundColor)) {
+        elements.foreGroundColorEl.style.borderRight = `60px solid ${foreGroundColor}`;
+        elements.backgroundColorEl.style.borderRight = `60px solid ${backgroundColor}`;
+        validColors = true;
+        elements.alertColorsEl.textContent = ":: valid colors";
+        elements.alertColorsEl.classList.add("alert-valid");
+        elements.alertColorsEl.classList.remove("alert-invalid");
+    } else {
+        elements.foreGroundColorEl.style.borderRight = "none";
+        elements.backgroundColorEl.style.borderRight = "none";
+        validColors = false;
+        elements.alertColorsEl.textContent = ":: invalid colors";
+        elements.alertColorsEl.classList.remove("alert-valid");
+        elements.alertColorsEl.classList.add("alert-invalid");
+    }
+}
+
+elements.urlInput.addEventListener("input", () => {
+    elements.alertUrlEl.classList.remove("hidden-none");
+    if (isValidUrl(elements.urlInput.value)) {
+        elements.alertUrlEl.textContent = ":: valid url";
+        elements.alertUrlEl.classList.add("alert-valid");
+        elements.alertUrlEl.classList.remove("alert-invalid");
+        siteUrl = elements.urlInput.value;
+        validURl = true;
+    } else {
+        elements.alertUrlEl.textContent = ":: invalid url";
+        elements.alertUrlEl.classList.remove("alert-valid");
+        elements.alertUrlEl.classList.add("alert-invalid");
+        siteUrl = "";
+        validURl = false;
+    }
+});
+
+elements.foreGroundColorEl.addEventListener("input", () => {
+    foreGroundColor = elements.foreGroundColorEl.value;
+    elements.alertColorsEl.classList.remove("hidden-none");
+    updateColors();
+});
+
+elements.backgroundColorEl.addEventListener("input", () => {
+    backgroundColor = elements.backgroundColorEl.value;
+    elements.alertColorsEl.classList.remove("hidden-none");
+    updateColors();
+});
+
+elements.generateBtnEl.addEventListener("click", () => {
+    if (validURl && validColors) {
+        generateQR(elements.qrEl, siteUrl, foreGroundColor, backgroundColor);
+        document.querySelector(".qrcode-container img").style.backgroundColor =
+            backgroundColor;
+        document.querySelector(
+            ".qrcode-container canvas"
+        ).style.backgroundColor = backgroundColor;
+        elements.ulrPreviewEl.innerHTML = `<a class="url" target="_blank" href="https://${siteUrl}">${siteUrl} &rarr;</a>`;
+    }
+});
+
+elements.themesEl.forEach((element) => {
+    element.addEventListener("click", () => {
+        foreGroundColor =
+            element.getElementsByTagName("span")[0].style.backgroundColor;
+        backgroundColor =
+            element.getElementsByTagName("span")[1].style.backgroundColor;
+        elements.foreGroundColorEl.value = foreGroundColor;
+        elements.backgroundColorEl.value = backgroundColor;
+        elements.foreGroundColorEl.style.borderRight = `60px solid ${foreGroundColor}`;
+        elements.backgroundColorEl.style.borderRight = `60px solid ${backgroundColor}`;
+    });
+});
+
+// elements.logoInputEl.onchange = () => {
+//     let reader = new FileReader();
+//     reader.readAsDataURL(elements.logoInputEl.files[0]);
+//     reader.onload = () => {
+//         clientLogo = reader.result;
+//     };
+// };
---
+++ functions.js
@@ -0,0 +1,56 @@
+const isValidUrl = (urlString) => {
+    var urlPattern = new RegExp(
+        "^(https?:\\/\\/)?" + // validate protocol
+            "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+            "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+            "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+            "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+            "(\\#[-a-z\\d_]*)?$",
+        "i"
+    ); // validate fragment locator
+    return !!urlPattern.test(urlString);
+};
+
+function isValidHex(hexCode) {
+    // Regular expression pattern for a valid hex code
+    var pattern = /#[0-9a-f]{6}|#[0-9a-f]{3}/gi;
+    return pattern.test(hexCode);
+}
+
+function generateQR(
+    qrCodeContainer,
+    url,
+    foregroundColor = "#000",
+    backgroundColor = "#fff"
+) {
+    qrCodeContainer.innerHTML = "";
+    qrcode = new QRCode(qrCodeContainer, {
+        text: url,
+        width: 500,
+        height: 500,
+        colorDark: foregroundColor,
+        colorLight: backgroundColor,
+        correctLevel: QRCode.CorrectLevel.H,
+    });
+}
+
+document.querySelector(".btn-download").addEventListener("click", async () => {
+    const qrImageSrc = document.querySelector(".qrcode-container img").src;
+
+    // Fetch the image
+    const response = await fetch(qrImageSrc);
+    const blob = await response.blob();
+
+    // Create a temporary anchor element
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = `QRapid-${siteUrl.toLowerCase().replaceAll(" ", "")}.png`;
+
+    // Simulate click on the anchor element to trigger download
+    document.body.appendChild(link);
+    link.click();
+
+    // Clean up
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+});
